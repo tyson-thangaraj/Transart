4775129d6667e76164818bc4ad70ff249a7ecdbb
Xiangting Fan <isfanxt@gmail.com>
2016-06-20 14:42:06 +0100

    delete

--- Financial_Eye/tweets/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.tweet_index, name='tweet_index'),
-    url(r'^tweet_list/$', views.tweet_list, name='tweet_list')
-
-]
