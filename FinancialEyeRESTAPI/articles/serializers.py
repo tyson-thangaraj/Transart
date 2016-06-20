04262116a63975cc536183742f823511fa8c7440
WinterJDWang <mrwangng@gmail.com>
2016-06-20 14:38:55 +0100

    delete first vision REST API

--- FinancialEyeRESTAPI/articles/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-# from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-from articles.models import  Article
-
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('Headline', 'SubHeadline', 'Url', 'DateTime', 'Keywords', 'Content', 'Type', 'Source')
-
