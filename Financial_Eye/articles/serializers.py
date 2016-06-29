9f12a0d23a4e07838066ce80f964b0df39c40821
WinterJDWang <mrwangng@gmail.com>
2016-06-29 16:49:03 +0100

    add image

--- Financial_Eye/articles/serializers.py
+++ Financial_Eye/articles/serializers.py
@@ -7,6 +7,6 @@ from articles.models import  Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ('Headline', 'SubHeadline', 'Url', 'DateTime', 'Keywords', 'Content', 'Type', 'Source')
+        fields = ('Headline', 'SubHeadline', 'Url', 'DateTime', 'Keywords', 'Content', 'Type', 'Source', 'Image')
 
 
