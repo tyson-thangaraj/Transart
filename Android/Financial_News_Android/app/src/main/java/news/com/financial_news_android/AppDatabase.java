b5fa93d989b058c95f3bc7ce9cceab286493ba71
alanhe87 <alanhe87@gmail.com>
2016-07-01 17:06:18 +0100

    register interface
    register function implementation

---
+++ Android/Financial_News_Android/app/src/main/java/news/com/financial_news_android/AppDatabase.java
@@ -0,0 +1,13 @@
+package news.com.financial_news_android;
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+/**
+ * Created by ping on 2016/6/30.
+ */
+@Database(name = AppDatabase.NAME, version = AppDatabase.VERSION)
+public class AppDatabase {
+    public static final String NAME = "AppDatabase";
+
+    public static final int VERSION = 1;
+}
