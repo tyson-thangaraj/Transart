6df986d682ccf8182a580adf769dde5aeefeb37a
Xiangting Fan <isfanxt@gmail.com>
2016-07-02 16:30:44 +0100

    data source

--- Financial_Eye/articles/function.py
+++ Financial_Eye/articles/function.py
@@ -45,6 +45,9 @@ def getArticleDetailsByUrl(url):
     news_content = article.text
     image = article.top_image
 
+    article.nlp()
+    keywords = article.keywords
+
     source = "other"
     if "chinadaily" in url:
         source = "ChinaDaily"
@@ -72,12 +75,52 @@ def getArticleDetailsByUrl(url):
         date = datetime.strptime(tag, "%Y%m%d%H%M%S")
     elif "reuters" in url:
         source="Reuters"
-    elif "ifeng" in url:
-        source = "Ifeng"
+    elif "huanqiu" in url:
+        source = "huanqiu"
     elif "sina" in url:
+        page = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(page,"html.parser")
+        soup.prettify()
         source = "Sina"
-
-    article.nlp()
-    keywords = article.keywords
+        news_content=''
+        for tag in soup.find("div", attrs={"id": "artibody"}).find_all("p"):
+            if "JavaScript" not in tag.get_text():
+                news_content += tag.get_text() + '\n'
+        keywords = extractKeywords(title)
 
     return [title, sub_title, news_content, date, keywords, source, image]
+
+def extractKeywords(text):
+    stop_words = load_stopwords()
+    keywords = []
+    tokens = nltk.word_tokenize(text)
+    #print(tokens)
+
+    for token in tokens:
+        if token not in stop_words:
+            keywords.append(token.lower())
+
+    #print(keywords)
+    return ", ".join(keywords)
+
+def load_stopwords():
+    stop_words = nltk.corpus.stopwords.words('english')
+    # custom stop words
+    stop_words.extend(['this', 'that', 'the', 'might', 'have', 'been', 'from',
+                           'but', 'they', 'will', 'has', 'having', 'had', 'how', 'went'
+                            'were', 'why', 'and', 'still', 'his','her',
+                           'was', 'its', 'per', 'cent',
+                           'a', 'able', 'about', 'across', 'after', 'all', 'almost', 'also', 'am', 'among',
+                           'an', 'and', 'any', 'are', 'as', 'at', 'be', 'because', 'been', 'but', 'by', 'can',
+                           'cannot', 'could', 'dear', 'did', 'do', 'does', 'either', 'else', 'ever', 'every',
+                           'for', 'from', 'get', 'got', 'had', 'has', 'have', 'he', 'her', 'hers', 'him', 'his',
+                           'how', 'however', 'i', 'if', 'in', 'into', 'is', 'it', 'its', 'just', 'least', 'let',
+                           'like', 'likely', 'may', 'me', 'might', 'most', 'must', 'my', 'neither', 'no', 'nor',
+                           'not', 'of', 'off', 'often', 'on', 'only', 'or', 'other', 'our', 'own', 'rather', 'said',
+                           'say', 'says', 'she', 'should', 'since', 'so', 'some', 'than', 'that', 'the', 'their',
+                           'them', 'then', 'there', 'these', 'they', 'this', 'tis', 'to', 'too', 'twas', 'us',
+                           'wants', 'was', 'we', 'were', 'what', 'when', 'where', 'which', 'while', 'who',
+                           'whom', 'why', 'will', 'with', 'would', 'yet', 'you', 'your', 've', 're', 'rt'])
+    #turn list into set for faster search
+    stop_words = set(stop_words)
+    return stop_words
