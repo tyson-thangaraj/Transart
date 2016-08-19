0e7135a06eb18838b4b53caab32d8ef2a6f6795c
WinterJDWang <mrwangng@gmail.com>
2016-08-19 15:13:53 +0100

    delete useless code

--- Financial_Eye/articles/views.py
+++ Financial_Eye/articles/views.py
@@ -36,32 +36,3 @@ class ArticleList(generics.ListCreateAPIView):
 class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-
-# def article_index(request):
-#     return HttpResponse("Hello, world. You're at the articles index view.")
-
-
-# def article_detail_id(request, article_id):
-#     article = Article.objects.filter(pk=article_id).first()
-#     if not article:
-#         return render(request, 'article_detail.html', {'error_message': "Article reading error."})
-
-#     else:
-#         return render(request, 'article_detail.html',
-#                       {'article': article})
-
-
-# def article_list(request):
-#     try:
-
-#         # th = datetime.now().replace(tzinfo=utc) - timedelta(hours=24*500)
-#         # print('-------------------------------------')
-#         # print(th)
-#         # list_of_articles = Article.objects.filter(DateTime__gte = th).order_by('-DateTime')
-#         list_of_articles = Article.objects.filter().order_by('-DateTime')
-
-#         return render(request, 'article_list.html',
-#                       {'latest_articles_list': list_of_articles, 'total': len(list_of_articles)})
-#     except:
-#         return render(request, 'article_list.html')
